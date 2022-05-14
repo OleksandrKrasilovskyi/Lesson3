323645762cd3ebf153dd5b87d6769a16803172ac
Oleksandr Krasilovskyi <sashikkrasilovskyi@gmail.com>
2022-05-14 16:53:03 +0300

    Fifth Task
    Signed-off-by: Oleksandr Krasilovskyi <sashikkrasilovskyi@gmail.com>

---
+++ src/com/company/Task5/Task5.java
@@ -0,0 +1,21 @@
+package com.company.Task5;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        final var objects = List.of(10, 4, 5, 6, 4, 9, 6);
+
+        System.out.println(findSquareAndDistinct(objects));
+    }
+
+    public static List<Integer> findSquareAndDistinct(final List<Integer> numbers) {
+
+        return numbers.stream()
+                .distinct()
+                .map(e -> e * e)
+                .collect(Collectors.toList());
+    }
+}
