f60573c4bc47ca7e3614eb1cca57d3e0bdbc607b
Oleksandr Krasilovskyi <sashikkrasilovskyi@gmail.com>
2022-05-14 15:21:43 +0300

    Second Task

---
+++ src/com/company/Task2/Task2.java
@@ -0,0 +1,21 @@
+package com.company.Task2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        final var list = List.of("one", "two", "three", "four", "five");
+
+        System.out.println(groupByStringLength(list));
+    }
+
+    public static Map<Integer, List<String>> groupByStringLength(final List<String> stringList) {
+
+        return stringList.stream()
+                .collect(Collectors.groupingBy(String::length, Collectors.mapping(Function.identity(), Collectors.toList())));
+    }
+}
