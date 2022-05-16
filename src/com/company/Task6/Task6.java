4d192dd22d9e5955e7d7d8a341480362c2275155
Oleksandr Krasilovskyi <sashikkrasilovskyi@gmail.com>
2022-05-16 15:59:51 +0300

    Sixth Task update

--- src/com/company/Task6/Task6.java
+++ src/com/company/Task6/Task6.java
@@ -2,18 +2,23 @@ package com.company.Task6;
 
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 
 public class Task6 {
     public static void main(String[] args) {
 
         final var number = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-        IntSummaryStatistics result = number.stream()
-                        .mapToInt(e -> e)
-                                .summaryStatistics();
 
-        System.out.println("Highest prime number in List : " + result.getMax());
-        System.out.println("Lowest prime number in List : " + result.getMin());
-        System.out.println("Sum of all prime numbers : " + result.getSum());
-        System.out.println("Average of all prime numbers : " + result.getAverage());
+        System.out.println("Highest prime number in List : " + intSummaryStatics(number).getMax());
+        System.out.println("Lowest prime number in List : " + intSummaryStatics(number).getMin());
+        System.out.println("Sum of all prime numbers : " + intSummaryStatics(number).getSum());
+        System.out.println("Average of all prime numbers : " + intSummaryStatics(number).getAverage());
+    }
+
+    public static IntSummaryStatistics intSummaryStatics(final List<Integer> numbers) {
+
+        return numbers.stream()
+                .mapToInt(e -> e)
+                .summaryStatistics();
     }
 }
